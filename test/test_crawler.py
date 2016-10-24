4c06c6d47f100a7e1c98a7274e17f93c8206c8e0
jinhwan byeon <jinhwan byeon>
2016-10-25 00:21:29 +0900

    whydda : 내 블로그 크롤링 하기 테스트 중 ~

--- test/test_crawler.py
+++ test/test_crawler.py
@@ -1,10 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def spider():
-        url = 'http://blog.naver.com/whydda'
+def spider(temp_url):
+        url = temp_url;
         source_code = requests.get(url)
         plain_text = source_code.text
-        #soup = BeautifulSoup(plain_text, 'lxml')
-        print(plain_text)
-spider()
+        soup = BeautifulSoup(plain_text, "lxml")
+        # print(soup)
+        for link in soup.select('p > a'):
+                href = link.get("href")
+                if(href != '#'):
+                        if(str(href).find("http://blog.naver.com/whydda") > -1):
+                            print(href)
+                            spider2(href)
+
+
+def spider2(temp_url):
+        url = temp_url;
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "lxml")
+        for link in soup.select("frameset > frame"):
+                src = link.get("src")
+                print(src)
+                if (str(src).find("/NBlogHidden.nhn?blogId=whydda&musicYN=false") < 0):
+                        spider('http://blog.naver.com' + src)
+
+spider('http://blog.naver.com/PostList.nhn?blogId=whydda&widgetTypeCall=true')
